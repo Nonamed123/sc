04fdc5a1a934cd03d4ccd1833b53139ae98b97c7
Nonamed <123anice@mail.ru>
2019-02-19 16:55:24 +0300

    Delete other elements

--- deploy1.sh
+++ deploy1.sh
@@ -3,8 +3,3 @@ set -e
 source /reddit
 bundle install
 rvm requirements
-sudo chmod 644 /lib/systemd/system/puma.service
-systemctl daemon-reload
-systemctl start puma
-systemctl enable puma
-sudo chmod u+x /usr/local/bin/puma
